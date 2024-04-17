661bbef3bec403903d776f2c6580399f0f3d5cc8
TapsHTS <tapsmc12@gmail.com>
2024-04-17 18:35:38 +0200

    blinds

--- index.js
+++ index.js
@@ -1,91 +1,115 @@
 import axios from 'axios'
 
 class HTTPDoor {
-  constructor (log, config, api) {
-    this.Service = api.hap.Service
-    this.Characteristic = api.hap.Characteristic
+    constructor(log, config, api) {
+        this.Service = api.hap.Service
+        this.Characteristic = api.hap.Characteristic
 
-    // valid types
-    this.types = ['lock', 'garage']
+        // valid types
+        this.types = ['lock', 'garage', 'blinds']
 
-    // config defaults
-    const defaults = {
-      method: 'GET',
-      autoLockTimeout: 5 * 1000,
-      type: 'lock'
+        // config defaults
+        const defaults = {
+            method: 'GET',
+            autoLockTimeout: 5 * 1000,
+            type: 'lock'
+        }
+
+        this.log = log
+        this.config = {...defaults, ...config }
+
+        // initialize service
+        if (this.config.type === 'lock') {
+            this.service = new this.Service.LockMechanism(this.config.name)
+        } else if (this.config.type === 'garage') {
+            this.service = new this.Service.GarageDoorOpener(this.config.name)
+        } else if (this.config.type === 'blinds') {
+            this.service = new this.Service.WindowCovering(this.config.name)
+        } else {
+            throw new Error('Invalid door type given')
+        }
     }
-
-    this.log = log
-    this.config = { ...defaults, ...config }
-
-    // initialize service
-    if (this.config.type === 'lock') {
-      this.service = new this.Service.LockMechanism(this.config.name)
-    } else if (this.config.type === 'garage') {
-      this.service = new this.Service.GarageDoorOpener(this.config.name)
-    } else {
-      throw new Error('Invalid door type given')
+    autoLock() {
+        if (this.autoLockTimeout) clearTimeout(this.autoLockTimeout)
+        this.autoLockTimeout = setTimeout(() => {
+            this.log.debug('Auto locking accessory')
+            this.updateState(1)
+        }, this.config.autoLockTimeout)
     }
-  }
-  autoLock () {
-    if (this.autoLockTimeout) clearTimeout(this.autoLockTimeout)
-    this.autoLockTimeout = setTimeout(() => {
-      this.log.debug('Auto locking accessory')
-      this.updateState(1)
-    }, this.config.autoLockTimeout)
-  }
-  updateState (currentState, targetState = null) {
-    if (this.config.type === 'lock') {
-      this.service.getCharacteristic(this.Characteristic.LockCurrentState).updateValue(currentState)
-      this.service.getCharacteristic(this.Characteristic.LockTargetState).updateValue(targetState === null ? currentState : targetState)
-    } else if (this.config.type === 'garage') {
-      this.service.getCharacteristic(this.Characteristic.CurrentDoorState).updateValue(currentState)
-      this.service.getCharacteristic(this.Characteristic.TargetDoorState).updateValue(targetState === null ? currentState : targetState)
+    updateState(currentState, targetState = null) {
+        if (this.config.type === 'lock') {
+            this.service.getCharacteristic(this.Characteristic.LockCurrentState).updateValue(currentState)
+            this.service.getCharacteristic(this.Characteristic.LockTargetState).updateValue(targetState === null ? currentState : targetState)
+        } else if (this.config.type === 'garage') {
+            this.service.getCharacteristic(this.Characteristic.CurrentDoorState).updateValue(currentState)
+            this.service.getCharacteristic(this.Characteristic.TargetDoorState).updateValue(targetState === null ? currentState : targetState)
+        } else if (this.config.type === 'blinds') {
+            this.service.getCharacteristic(this.Characteristic.CurrentPosition).updateValue(currentState)
+            this.service.getCharacteristic(this.Characteristic.TargetPosition).updateValue(targetState === null ? currentState : targetState)
+        }
     }
-  }
-  handleSetState (value, callback) {
-    if (this.config.type === 'garage') this.updateState(2, 0)
-    return axios({
-      method: this.config.method,
-      url: this.config.url
-    }).then(() => {
-      this.log.debug('Unlocking accessory')
-      this.updateState(0)
-      this.autoLock()
-      callback()
-    }).catch(err => {
-      this.log.warn('HTTP request failed', err.message)
-      this.updateState(new Error('HTTP request failed'))
-      this.autoLock()
-      callback(err)
-    })
-  }
-  getServices () {
-    // initialize information service
-    this.informationService = new this.Service.AccessoryInformation()
-    this.informationService.setCharacteristic(this.Characteristic.Manufacturer, 'eo')
-
-    let characteristic
-    if (this.config.type === 'lock') {
-      characteristic = this.Characteristic.LockTargetState
-    } else if (this.config.type === 'garage') {
-      characteristic = this.Characteristic.TargetDoorState
-    } else {
-      throw new Error('Invalid door type given')
+    handleSetState(value, callback) {
+        if (this.config.type === 'garage') {
+            this.updateState(2, 0)
+            return axios({
+                method: this.config.method,
+                url: this.config.url
+            }).then(() => {
+                this.log.debug('Unlocking accessory')
+                this.updateState(0)
+                this.autoLock()
+                callback()
+            }).catch(err => {
+                this.log.warn('HTTP request failed', err.message)
+                this.updateState(new Error('HTTP request failed'))
+                this.autoLock()
+                callback(err)
+            })
+        } else if (this.config.type === 'blinds') {
+            this.updateState(value)
+            return axios({
+                method: this.config.method,
+                url: this.config.url
+            }).then(() => {
+                this.log.debug('Setting accessory state to', value)
+                this.autoLock()
+                callback()
+            }).catch(err => {
+                this.log.warn('HTTP request failed', err.message)
+                this.updateState(new Error('HTTP request failed'))
+                this.autoLock()
+                callback(err)
+            })
+        }
     }
+    getServices() {
+        // initialize information service
+        this.informationService = new this.Service.AccessoryInformation()
+        this.informationService.setCharacteristic(this.Characteristic.Manufacturer, 'eo')
 
-    this.service
-      .getCharacteristic(characteristic)
-      .on('set', this.handleSetState.bind(this))
+        let characteristic
+        if (this.config.type === 'lock') {
+            characteristic = this.Characteristic.LockTargetState
+        } else if (this.config.type === 'garage') {
+            characteristic = this.Characteristic.TargetDoorState
+        } else if (this.config.type === 'blinds') {
+            characteristic = this.Characteristic.TargetPosition
+        } else {
+            throw new Error('Invalid door type given')
+        }
 
-    // set initial state
-    this.log.debug('Setting initial accessory state')
-    this.updateState(1)
+        this.service
+            .getCharacteristic(characteristic)
+            .on('set', this.handleSetState.bind(this))
 
-    return [this.informationService, this.service]
-  }
+        // set initial state
+        this.log.debug('Setting initial accessory state')
+        this.updateState(1)
+
+        return [this.informationService, this.service]
+    }
 }
 
-export default function (homebridge) {
-  homebridge.registerAccessory('@eymengunay/homebridge-http-door', 'HTTPDoor', HTTPDoor)
+export default function(homebridge) {
+    homebridge.registerAccessory('@tapshts/homebridge-http-door', 'HTTPDoor', HTTPDoor)
 }
